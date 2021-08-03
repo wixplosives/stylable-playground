18d2d79bbb853eccbcfc28189d00e8b0b2d55de6
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-08-03 14:07:48 +0300

    feat: color theme (#14)

---
+++ src/lib/use-color-theme.ts
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react';
+import { classes } from '../components/app.st.css';
+
+const mediaDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+export function useColorTheme() {
+    const [theme, setTheme] = useState<'dark' | 'light'>(
+        mediaDarkScheme.matches ? 'dark' : 'light'
+    );
+
+    useEffect(() => {
+        const callback = (e: MediaQueryListEvent) => {
+            setTheme(e.matches ? 'dark' : 'light');
+        };
+
+        mediaDarkScheme.addEventListener('change', callback);
+
+        return () => {
+            mediaDarkScheme.removeEventListener('change', callback);
+        };
+    }, []);
+
+    return {
+        theme,
+        themeClassName: classes[`${theme}Theme`],
+    };
+}
