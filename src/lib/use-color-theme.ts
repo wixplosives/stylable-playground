0d1e08cfb5d62885a77cf5cb1b9e6cf82c15b8d1
Tzach Bonfil <45866571+tzachbon@users.noreply.github.com>
2021-08-03 10:58:09 +0300

    Use const media query

--- src/lib/use-color-theme.ts
+++ src/lib/use-color-theme.ts
@@ -1,17 +1,26 @@
 import { useEffect, useState } from 'react';
-import { classes } from '../components/app.st.css'
+import { classes } from '../components/app.st.css';
 
+const mediaDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
 export function useColorTheme() {
-  const [theme, setTheme] = useState<'dark' | 'light'>('dark')
-  
-  useEffect(() => {
-    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', e => {
-      setTheme(e.matches ? "dark" : "light")
-    });
-  }, [])
+    const [theme, setTheme] = useState<'dark' | 'light'>(
+        mediaDarkScheme.matches ? 'dark' : 'light'
+    );
 
-  return {
-    theme,
-    themeClassName: classes[`${theme}Theme`]
-  }
+    useEffect(() => {
+        const callback = (e: MediaQueryListEvent) => {
+            setTheme(e.matches ? 'dark' : 'light');
+        };
+
+        mediaDarkScheme.addEventListener('change', callback);
+
+        return () => {
+            mediaDarkScheme.removeEventListener('change', callback);
+        };
+    }, []);
+
+    return {
+        theme,
+        themeClassName: classes[`${theme}Theme`],
+    };
 }
